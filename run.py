2bf3dce1a74c165d37cb22e2f01a5a2db8413332
jakedolan443 <jakedolan443@protonmail.com>
2021-09-16 17:07:27 +0100

    update IP and port

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from main import app
 
-http_server = WSGIServer(('127.0.0.1', 16100), app)
+http_server = WSGIServer(('0.0.0.0', 80, app)
 http_server.serve_forever()
