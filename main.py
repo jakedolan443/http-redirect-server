1de54f46cb3951ab44e22777dd7fab3f4c2205a4
retro <jakedolan443@protonmail.com>
2021-09-16 17:02:33 +0100

    added files

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect
+
+
+app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+
