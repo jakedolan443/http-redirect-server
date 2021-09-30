5e87e40666c3783b9c4f7ba20610370f0047c7fc
jakedolan443 <jakedolan443@protonmail.com>
2021-09-30 22:51:20 +0100

    fixed minor syntax error

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from main import app
 
-http_server = WSGIServer(('0.0.0.0', 80, app)
+http_server = WSGIServer(('0.0.0.0', 80), app)
 http_server.serve_forever()
